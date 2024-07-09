6a798f4fd9ea641fe96daaac111e972b55200a09
Gaurav Bhardwaj <96366072+BhardwajG572@users.noreply.github.com>
2024-07-09 02:50:04 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,54 @@
+//" use strict "; // treat all JS code as newer version 
+// this is signifying that , we are using newer version of JS code style
+// where we have class, module, objects, etc
+
+// alert(3+3) //  work fine in developer console(browser )
+// here this would give me error, as we know whole JS engine is hidden in  browser.
+// we are using nodejs, not browser.
+
+console.log(3 + 3) 
+console.log("gaurav") // make readable code.
+
+// code readability should be high
+
+// mdn documentation >>> for reading
+// tc39.es >> ecma(organisation that set standards) >> for official JS standards
+
+// data types
+
+let name = "gaurav"
+let age = 32          
+// number (range )>> 2 to power 53
+let isLoggedIn = true
+/*
+// boolean 
+// bigint 
+// string => ""
+// boolean => true/false
+// null : standalone value 
+// undefined : variable made , but value not given  ( for eg: let state; 
+
+in undefined : value is not defined yet.
+
+> symbol : (to find uniqueness) ( mostly used in React)
+
+
+
+// object
+
+
+
+
+
+
+
+
+
+
+*/
+
+console.log(typeof("gaurav"))
+console.log(typeof null) // return object
+
+console.log(typeof undefined) // return undefined 
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,27 @@
 console.log("Gaurav Bhardwaj")
+console.log("GauravBhardwaj")
+console.log("Gaurav Bhardwaj")
+
+const account_id = 83928
+let account_email= "gaurav952@gmail.com"
+var account_password = "29839"
+
+account_city = "Patna"
+let account_state;
+
+// account_id = 283928 // not possible(become immutable) in javascript,once it become const
+
+account_email="rudra@gmail.com"
+account_password="9328329"
+
+account_city="Ranchi"
+
+console.log(account_id)
+
+console.table([account_id,account_email,account_city,account_password,account_city])
+
+// only let is used , no var is used 
+/*
+ prefer not to use var
+ because of issue in block  scope and functional scope
+  */
